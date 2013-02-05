c3bd81a833792379a252d1d4d7e81658e3917c73
saasbook <saasbook@saasbook.(none)>
2013-02-05 12:02:49 -0800

    final submission

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,28 +1,49 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  Movie.destroy_all
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+	#puts "e1:" + page.body.index(e1).to_s
+	#puts "e2:"+ page.body.index(e2).to_s
+	page.body.index(e1).should < page.body.index(e2)
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.gsub(/ /, '').split(",").each do |rating| 
+	uncheck ? uncheck("ratings_" + rating) : check("ratings_" + rating);
+  end
+end
+
+When /I press (.*) button/ do |button|
+	click_button("ratings_" + button)
+end
+
+Then /I should see that (.*) movies are( not)? visible/ do |rating_list, is_or_not|
+	rating_list_db = page.all(:xpath, '//table[@id="movies"]/tbody/tr/td[2]')
+	rating_list.gsub(/ /, '').split(",").each do |rating| 
+		count=0
+		rating_list_db.each do |rating_db|
+			#puts rating_db.text + ", " + rating
+			if rating_db.text == rating
+				count+=1
+			end
+		end
+		is_or_not ? count.should == 0 : count.should > 0
+		#puts("Total for " + rating + ":" + count.to_s )
+	end
+end
+
+Then /I should see all of the movies/ do 
+	countHTML = page.all(:xpath, '//table[@id="movies"]/tbody/tr').count
+	countDB =  Movie.count
+	#puts("CountHTML:" + countHTML.to_s  + ", countDB" + countDB.to_s )
+	countHTML.should == countDB
 end
